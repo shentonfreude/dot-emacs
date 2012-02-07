99c014395aef65bed31450d583d6b5e47175bc8d
Chris Shenton <chris@koansys.com>
2012-02-06 19:17:23 -0500

    For satellite tracker MATLAB code

---
+++ init-octave.el
@@ -0,0 +1,6 @@
+;; (autoload 'matlab-mode "matlab" "Matlab Editing Mode" t)
+ (add-to-list
+  'auto-mode-alist
+  '("\\.m$" . octave-mode))
+;; (setq matlab-indent-function t)
+;; (setq matlab-shell-command "matlab")
