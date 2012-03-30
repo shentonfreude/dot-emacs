0802d7c92e0ac14479f577a69f3818da25bd37d0
Chris Shenton <chris@koansys.com>
2012-03-30 11:27:21 -0400

    Fix mode selection so css-mode actually fires

--- init-css.el
+++ init-css.el
@@ -1,4 +1,7 @@
-(autoload 'css-mode "css" nil t)
+;; why is it not loading/finding css-mode from 
+;; /Applications/Emacs.app/Contents/Resources/lisp/textmodes/css-mode.el.gz
+
+(autoload 'css-mode "css-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.less$" . css-mode))
 
