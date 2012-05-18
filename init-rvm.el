73149998c39ef6cf643b0b65be63da9076f44c82
Chris Shenton <chris@koansys.com>
2012-05-18 14:06:10 -0400

    Add Ruby Virtualenv Manager for hacking cloudfoundry (?) and others

---
+++ init-rvm.el
@@ -0,0 +1,4 @@
+;;; 2012-05-18 DON'T USE: fucks with path removing /usr/local/bin/
+;;/Users/chris/.rvm/gems/ruby-1.9.2-p318/bin:/Users/chris/.rvm/gems/ruby-1.9.2-p318@global/bin:/Users/chris/.rvm/rubies/ruby-1.9.2-p318/bin/:/usr/bin:/bin:/usr/sbin:/sbin
+;;(require 'rvm)
+;;(rvm-use-default) ;; use rvm's default ruby for the current Emacs session
