9de87e637bc9fa4d8ccb5739d0aaa9f36487c2cb
Chris Shenton <chris@koansys.com>
2014-12-09 18:21:38 -0500

    Only set path on OSX since it hangs FreeBSD

--- init-1-exec-path-from-shell.el
+++ init-1-exec-path-from-shell.el
@@ -2,6 +2,9 @@
 ;; but OSX launchers get the default PATH, 
 ;; so miss custom stuff in my ~/.bash_profile
 ;; Do this early so other packges find things; is this needed?
+;; Only do this on OS X, it hangs on FreeBSD
 
-(require 'exec-path-from-shell) ;; if not using the ELPA package
-(exec-path-from-shell-initialize)
+(if (eq system-type 'darwin)
+    ((require 'exec-path-from-shell) ;; if not using the ELPA package
+     (exec-path-from-shell-initialize)
+     ))
