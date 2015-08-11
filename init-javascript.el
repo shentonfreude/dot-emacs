13b18933d2aa4239ed0b85f0edacc0b9d00dfa13
Chris Shenton <chris@shenton.org>
2015-08-11 16:06:38 +0200

    line numbers in javascript; flycheck working fine

--- init-javascript.el
+++ init-javascript.el
@@ -1,18 +1,23 @@
+;;; Package -- summary
+;;; Commentary:
 ;;; Old espresso mode is now built-in javascript mode, js-mode.
 ;;; IMHO the js2 mode is too heavy handed and comlex and slow.
-;;; TODO: how to trim trailing whitespace?
+;;; This is executing FlyCheck fine now, but without trailing white complaints.
+;;; TODO:
+;;; - how to trim trailing whitespace?
+;;; (defun before-save-hook ()
+;;;   (delete-trailing-whitespace)
+;;;   )
 
-;; (defun before-save-hook ()
-;;   (delete-trailing-whitespace)
-;;   )
+;;; Code:
 
-;; We get .js -> js-mode (not javascript-mode) for free, so add .json.
-;; Why does the var have .js with squote instead of dollar?
-;; ("\\.js\\'" . js-mode)
-;; TODO: Can't get flymake to run with json anyway, why?? inappropriate?
+;; javascript-mode is free, so do it for .json.
 (add-to-list 'auto-mode-alist '("\\.json\\'" . js-mode))
 
-;; (require 'flymake-node-jshint)
-;; (add-hook 'javascript-mode-hook
-;;  	  (lambda () (flymake-mode t)))
+(add-hook 'javascript-mode-hook
+          (linum-mode t)
+          (setq show-trailing-whitespace t)
+          )
 
+(provide 'init-javascript)
+;;; init-javascript.el ends here
