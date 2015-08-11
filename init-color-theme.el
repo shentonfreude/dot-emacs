6057093b17b52b08bbe36766e49f1df5e3669bc2
Chris Shenton <chris@shenton.org>
2015-08-11 16:06:22 +0200

    Need fringe to help determine starting column

--- init-color-theme.el
+++ init-color-theme.el
@@ -21,7 +21,7 @@
 ;;; SOLARIZED: Do these before load-theme solarized:
 ;;
 ;; make the fringe stand out from the background
-;;(setq solarized-distinct-fringe-background t)
+(setq solarized-distinct-fringe-background t)
 ;; Don't change the font for some headings and titles
 ;;(setq solarized-use-variable-pitch nil)
 ;; make the modeline high contrast
@@ -29,7 +29,7 @@
 ;; Use less bolding
 ;;(setq solarized-use-less-bold t)
 ;; Use more italics
-;;(setq solarized-use-more-italic t)
+(setq solarized-use-more-italic t)
 ;; Use less colors for indicators such as git:gutter, flycheck and similar
 ;;(setq solarized-emphasize-indicators nil)
 ;; Don't change size of org-mode headlines (but keep other size-changes)
