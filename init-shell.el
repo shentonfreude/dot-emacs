f2b2e07ea4877cf2a18d409293f6b27dbf3a2e17
Chris Shenton <chris@v-studios.com>
2016-06-30 11:58:41 -0400

    This no longer seems to be working
    When I first used it, I must have had a python buffer and it worked
    great. When I restarted emacs, I did not, and opened a shell and it
    complained it didn't recognize python-pdbtrack-... so I added the
    require python.(

--- init-shell.el
+++ init-shell.el
@@ -1,8 +1,10 @@
 ;;; package -- Make shell track python pdb so we can debug proc with embedded pdb
 ;;; Commentary:
+;;; Should this go into init-python.el instead?
 
 ;;; Code:
 
+(require 'python)                       ; to get the python-* function
 (defun my-shell-mode-hook ()
   (add-hook 'comint-output-filter-functions 'python-pdbtrack-comint-output-filter-function t))
 
