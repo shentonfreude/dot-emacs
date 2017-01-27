ae57eb2da562e9e3adce237ee559419f20b945c1
Chris Shenton <chris@v-studios.com>
2017-01-27 10:39:31 -0500

    Turn on colors in shell

--- init-shell.el
+++ init-shell.el
@@ -6,7 +6,9 @@
 
 (require 'python)                       ; to get the python-* function
 (defun my-shell-mode-hook ()
-  (add-hook 'comint-output-filter-functions 'python-pdbtrack-comint-output-filter-function t))
+  (add-hook 'comint-output-filter-functions 'python-pdbtrack-comint-output-filter-function t)
+  (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+  )
 
 (add-hook 'shell-mode-hook 'my-shell-mode-hook)
 
