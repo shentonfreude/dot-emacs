4786b829fe5b4ab132596fee17bff40437b7f551
Chris Shenton <chris@koansys.com>
2017-01-30 09:52:00 -0500

    Move lisp to own dir to quiet emacs warning

--- init.el
+++ init.el
@@ -1,7 +1,18 @@
+;;; package -- emacs configuration
+;;; Commentary:
 ;;; Formerly ~/.emacs for Carbon Emacs, but also ready by AquaEmacs.
 ;;; AquaEmacs prefers ~/Library/Preferences/Aquamacs Emacs/Preferences.el
 
-(setq emacs-dir "~/.emacs.d")
+
+;;; Code:
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(defvar emacs-dir)
+(setq emacs-dir "~/.emacs.d/lisp")      ;warning says not to use .emacs.d
 (add-to-list 'load-path emacs-dir)
 
 (setq mac-option-modifier nil)		;for international chars like ü
