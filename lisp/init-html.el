c70d01de5d4b3b9fa2cf8c9e66ce66251de26c0d
Chris Shenton <chris@v-studios.com>
2017-04-26 22:43:30 -0400

    Chameleon templates use web-mode instead of XML

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -1,3 +1,4 @@
 ;; html-mode is built-in, so don't try and load it.
 (add-to-list 'auto-mode-alist '("\\.asp$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.pt$" . web-mode))
