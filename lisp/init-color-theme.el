e5b5233d3b563e17b52739c0ede3a9f173ac613c
Chris Shenton <chris.shenton@nasa.gov>
2019-03-26 13:12:46 -0400

    Make it easier to read, find cursor on big frame

--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -13,7 +13,7 @@
 (blink-cursor-mode 1)			;-1 off, 1 on
 (setq blink-cursor-interval 0.25)	;default is 0.5 seconds
 (setq blink-cursor-delay 1)             ;default 0.25
-
+(setq blink-cursor-blinks 1000)         ;stop after 1000 blinks, default 10 too short
 
 ;;; init-color-theme.el ends here
 
