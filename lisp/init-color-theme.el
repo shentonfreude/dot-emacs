1ccf8fb0ca14411a40dbfc2953d9e292bf21d1fa
Chris Shenton <chris.shenton@nasa.gov>
2019-04-04 08:58:21 -0400

    Make cursor line more visible with hilight

--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -9,11 +9,13 @@
 
 ;; Customize
 
+                                        ; yellow, chartreuse, magenta, ...
 (set-cursor-color "magenta")            ; material default orange hard to find
 (blink-cursor-mode 1)			;-1 off, 1 on
 (setq blink-cursor-interval 0.25)	;default is 0.5 seconds
 (setq blink-cursor-delay 1)             ;default 0.25
 (setq blink-cursor-blinks 1000)         ;stop after 1000 blinks, default 10 too short
+(global-hl-line-mode t)                 ;hilight active buffer line
 
 ;;; init-color-theme.el ends here
 
