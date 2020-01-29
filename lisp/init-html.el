468816c28321a8e72b06ecf91ce8633e9e0a6d09
Chris Shenton <chris@v-studios.com>
2020-01-29 17:46:41 -0500

    Make HTML angle brackets visible on dark Material theme

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -2,3 +2,15 @@
 (add-to-list 'auto-mode-alist '("\\.asp$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.pt$" . web-mode))
+
+
+;; surely there is a sane set that looks good against my 'material' theme
+;; but the defaults with 'material' make angle brackets impossible to read
+;; make them same as tag color for now
+;;
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "Orange")
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
