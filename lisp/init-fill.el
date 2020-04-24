3cca4e681a9ad2b208d1df347295beaeba8df869
Chris Shenton <chris@v-studios.com>
2020-04-24 14:46:23 -0400

    Use single space after period when filling text

---
+++ lisp/init-fill.el
@@ -0,0 +1,10 @@
+;;; package -- fill settings
+;;; Commentary:
+;;; random settings
+;;; Code:
+
+(set-variable 'sentence-end-double-space nil) ;break after period and one space
+
+(provide 'init-fill)
+;;; init-fill ends here
+
