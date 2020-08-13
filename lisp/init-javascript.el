95e4c58c299f8ef6b1d30c14cc7bb493d94d289f
Chris Shenton <chris.shenton@nasa.gov>
2020-08-13 11:48:38 -0400

    fix javascript indenting -- old hook was wrong

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -1,23 +1,19 @@
 ;;; Package -- summary
 ;;; Commentary:
-;;; Old espresso mode is now built-in javascript mode, js-mode.
 ;;; IMHO the js2 mode is too heavy handed and comlex and slow.
 ;;; This is executing FlyCheck fine now, but without trailing white complaints.
-;;; TODO:
-;;; - how to trim trailing whitespace?
-;;; (defun before-save-hook ()
-;;;   (delete-trailing-whitespace)
-;;;   )
 
 ;;; Code:
 
 ;; javascript-mode is free, so do it for .json.
 (add-to-list 'auto-mode-alist '("\\.json\\'" . js-mode))
 
-(add-hook 'javascript-mode-hook
-          (linum-mode t)
-          (setq show-trailing-whitespace t)
-          (setq js-indent-level 2)
+(add-hook 'js-mode-hook
+          (lambda ()
+            (setq show-trailing-whitespace t)
+            (setq js-indent-level 2)    ; better with code's .editorconfig
+            (linum-mode t)
+            )
           )
 
 (provide 'init-javascript)
