d0a92efe8b90da2dd73cc23394c5a9b54192490a
Chris Shenton <chris@v-studios.com>
2021-01-22 13:25:38 -0500

    Don't try to RST .txt files, typically not appropriate

--- lisp/init-rst.el
+++ lisp/init-rst.el
@@ -1,13 +1,18 @@
-;;; RST
+;;; package -- Summary: RST processing of .rst files
 
+;;; Commentary:
+;;; We don't try to RST .txt files because they're too generic
+
+;;; Code:
 (autoload 'rst-mode "rst" nil t)
 (add-to-list 'auto-mode-alist '("\\.rst$" . rst-mode))
 (add-to-list 'auto-mode-alist '("\\.rest$" . rst-mode))
-(add-to-list 'auto-mode-alist '("\\.txt$" . rst-mode))
-
 
 (defun my-whitespace-killer-hook ()
   (setq show-trailing-whitespace t)     ; color set in init.el
   )
 (add-hook 'rst-mode-hook 'my-whitespace-killer-hook)
 
+
+(provide 'init-rst)
+;;; init-rst.el ends here
